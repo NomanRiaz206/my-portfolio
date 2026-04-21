5e02d4f490ec329cb8458e4341e5e6b449bfa283
NomanRiaz206 <nomanriaz.dev@gmail.com>
2026-04-21 15:06:47 +0500

    Redis installation

--- dockerfile
+++ dockerfile
@@ -12,6 +12,9 @@ RUN apt-get update && apt-get install -y \
     default-mysql-client \
     && docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
 
+    
+RUN pecl install redis && docker-php-ext-enable redis
+
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 WORKDIR /var/www
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
  <footer class="border-t border-white/10">
         <div class="mx-auto flex max-w-7xl flex-col gap-4 px-4 py-6 text-sm text-white/42 sm:flex-row sm:items-center sm:justify-between sm:px-6 lg:px-8">
-            <p>© {{ date('Y') }} Ali Hassan Cheema. All rights reserved.</p>
+            <p>© {{ date('Y') }} Noman Riaz. All rights reserved.</p>
             <p>Built with Laravel & Tailwind CSS</p>
         </div>
     </footer>
